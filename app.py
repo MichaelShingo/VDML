c5ff3a01b1a197178130f599fd71464f07602165
MichaelShingo <mcrawford5376@gmail.com>
2023-01-20 13:01:49 -0500

    completed late_equipment page

--- app.py
+++ app.py
@@ -1,19 +1,62 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import request
 import os
 from werkzeug.utils import secure_filename
+from scripts import lateEquipment
+
+# virtualenv env 
+# source/env/bin/activate
+# TODO you might have to install pyperclip in env
+
 
 UPLOAD_FOLDER = './uploads/'
 ALLOWED_EXTENSIONS = {'.csv'}
+
 app = Flask(__name__) #references this file
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')  #routes to index.html so you don't 404
+def index():
+    return render_template('index.html')
+
+
+@app.route("/late_equipment")
+def late_equipment(visibility='hidden'):
+    return render_template('late_equipment.html', visibility='hidden')
+
+@app.route("/late_equipment", methods=['POST'])
+def late_equipment_post(resultText=None, visibility='hidden'):
+    text = request.form['text']
+    processed_text = lateEquipment.generateEmail(text)
+    return render_template('late_equipment.html', resultText=processed_text, methods=['POST'], visibility='visible')
+
+@app.route("/late_fines")
+def late_fines(visibility='hidden', cols='0'):
+    return render_template('late_fines.html', cols='0', visibility='hidden')
+
+
 
 def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')  #routes to index.html so you don't 404
 
-@app.route('/', methods=['GET', 'POST'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/booking_analysis', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         #check if the post request has the file part
@@ -40,9 +83,6 @@ def upload_file():
     </form>
     '''
 
-def index():
-    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == "__main__": # watches for changes and updates
     app.run(debug=True)
-
---
+++ scripts/lateEquipment.py
@@ -0,0 +1,63 @@
+import re
+import pyperclip
+
+def generateEmail(text):
+    bookingRegex = re.compile(r'Con\d{6}')
+    bookingNum = bookingRegex.findall(text)
+    bookingNum = bookingNum[0]
+    dateRegex = re.compile(r'\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d\d \w\w - \d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d\d \w\w')
+    dateRange = dateRegex.findall(text)
+
+    if len(dateRange) == 0:
+        dateRegex = re.compile(r'\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d{2} \w\w - \d{1,2}:\d{2} \w\w')
+        dateRange = dateRegex.findall(text)
+
+    dateRange = dateRange[0]
+
+    ownerIndex = text.index('Owner')
+    ownerIndex += 6
+    ownerEnd = text.find('(') - 1
+    owner = text[ownerIndex:ownerEnd]
+    owner = owner.upper().strip()
+
+
+    dateRangeIndex = text.index(dateRange)
+    dateRangeIndex += len(dateRange)
+    text = text[dateRangeIndex + 3:]
+
+
+    barcodeRegex = re.compile(r'\t\d{4}\t') #gets all barcodes
+    barcodeList = barcodeRegex.findall(text)
+    firstBarcodeIndex = text.index(barcodeList[0])
+
+    firstEquipment = text[:firstBarcodeIndex]
+    equipmentList = [firstEquipment]
+
+
+    equipmentRegex = re.compile(r'\t.{5,}\t') #gets all equipment except first one
+    equipmentList.extend(equipmentRegex.findall(text))
+
+    for i in range(1, len(equipmentList)):
+        equipmentList[i] = equipmentList[i][:len(equipmentList[i]) - 6]
+
+    for i in range(len(equipmentList)):
+        equipmentList[i] = equipmentList[i].strip()
+    equipmentString = '\n'.join(equipmentList)
+    print(owner, dateRange, bookingNum)
+    resultString = """Dear %s,
+
+Our records show that you have equipment still checked out from us.
+
+Please return our equipment or the fine will continue to accumulate and will be issued onto your Penn library account.
+
+-------------------------------------------------------------
+%s
+%s
+Items:
+%s
+-------------------------------------------------------------
+
+
+Let us know if you have any concerns or questions.""" % (owner, bookingNum, dateRange, equipmentString)
+    pyperclip.copy(resultString)
+    return resultString
