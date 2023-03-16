488cfbcfeaca4196608482bdf715ad990cf293c2
MichaelShingo <mcrawford5376@gmail.com>
2023-03-16 09:26:15 -0400

    changed files directory path

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ app.config['UPLOAD_FOLDER'] = 'static/files'
 app.config['SECRET_KEY'] = 'jj8^^83jd)))ueid9ieSHI!!'
 db.init_app(app)
 
-FILES_DIRECTORY = os.path.join(os.path.dirname(__file__), app.config['UPLOAD_FOLDER'])
+FILES_DIRECTORY = os.path.join(os.path.dirname(__file__), '/static/files/')
 
 class UploadFileForm(FlaskForm): #FlaskForm module for booking_analysis page
     file = FileField('File', validators=[InputRequired()])
